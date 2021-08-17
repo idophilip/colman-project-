7f6d5935b8e90edef623683471ab2e687205515d
dan feithlicher <d.feithlicher@gmail.com>
2021-08-17 21:57:05 +0300

    add login model

---
+++ ColmanJira/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(ColmanJira.Areas.Identity.IdentityHostingStartup))]
+namespace ColmanJira.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+            });
+        }
+    }
+}
